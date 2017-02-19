5af3f21c7eefda122dc7fc7ec29bd00f997f924d
Surakit Choodet <kuakling@gmail.com>
2017-02-19 09:36:34 +0700

    Update DatePicker.php

--- DatePicker.php
+++ DatePicker.php
@@ -1,12 +1,14 @@
 <?php
 namespace kuakling\datepicker;
+
+use yii\bootstrap\Html;
 /**
  * \kuakling\datepicker\DatePicker.
  *
  * @author kuakling <kuakling@gmail.com>
  * @since 1.0
  */
-class DatePicker extends \yii\base\Widget
+class DatePicker extends \yii\widgets\InputWidget
 {
     public $options = [];
     /**
@@ -21,24 +23,29 @@ class DatePicker extends \yii\base\Widget
     
     protected function renderInput()
     {
-        retutn '<div class="input-group">
-    <input type="text" class="form-control" id="test-date">
-    <span class="input-group-btn">
-        <button type="button" class="btn btn-default"><i class="glyphicon glyphicon-calendar"></i></button>
-    </span>
-</div>';
+        $options = array_replace_recursive($this->options, ['class' => 'form-control']);
+        $html = '<div class="input-group">';
+        $html .= $this->hasModel() ?
+            Html::activeTextInput($this->model, $this->attribute, $options) :
+            Html::textInput($this->name, $this->value, $options);
+        $html .= '<span class="input-group-addon" style="cursor: pointer;" onclick="$(this).closest(\'.input-group\').find(\'input[type=text]\').focus()">';
+        $html .= '<i class="glyphicon glyphicon-calendar"></i>';
+        $html .= '</span>';
+        $html .= '</div>';
+        echo $html;
     }
     
     public function registerAsset($asset)
     {
         $view = $this->getView();
+        $inputId = $this->options['id'];
         $options = array_merge([
             'language' => 'th-th',
             'autoclose' => true
         ], $this->options);
         $jsonOptions = \yii\helpers\Json::encode($options);
         $js[] = <<< JS
-$('#test-date').datepicker(jsonOptions);
+$("#{$inputId}").datepicker($jsonOptions);
 JS;
         $view->registerJs(implode("\n", $js));
     }
