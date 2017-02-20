0d009c400b6276b09db01f3994bf63ec93a9c594
Surakit Choodet <kuakling@gmail.com>
2017-02-20 18:22:24 +0700

    Update DatePicker.php

--- DatePicker.php
+++ DatePicker.php
@@ -1,6 +1,7 @@
 <?php
 namespace kuakling\datepicker;
 
+use Yii;
 use yii\bootstrap\Html;
 /**
  * \kuakling\datepicker\DatePicker.
@@ -24,10 +25,20 @@ class DatePicker extends \yii\widgets\InputWidget
     protected function renderInput()
     {
         $options = array_replace_recursive($this->options, ['class' => 'form-control']);
+        $valTh = $this-
         $html = '<div class="input-group">';
-        $html .= $this->hasModel() ?
-            Html::activeTextInput($this->model, $this->attribute, $options) :
-            Html::textInput($this->name, $this->value, $options);
+        if($this->hasModel()){
+            
+            $val = $this->model->isNewRecord ? null : $this->model->{$this->attribute};
+            $date = \DateTime::createFromFormat('Y-m-d', $val);
+            $this->value = $date ? Yii::$app->formatter->asDate($val, 'php:d/m/Y') : null;
+            
+            //$html .= Html::activeTextInput($modelTemp, $this->attribute, $options) ;
+            $this->name = Html::getInputName($this->model, $this->attribute);
+            $html .= Html::textInput($this->name, $this->value, $options);
+        }else{
+            $html .= Html::textInput($this->name, $this->value, $options);
+        }
         $html .= '<span class="input-group-addon" style="cursor: pointer;" onclick="$(this).closest(\'.input-group\').find(\'input[type=text]\').focus()">';
         $html .= '<i class="glyphicon glyphicon-calendar"></i>';
         $html .= '</span>';
