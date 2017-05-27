1dae15ec56b2e6830d6b5a2467ab8e9ccc90eab9
Surakit Choodet <surakit@Surakits-MacBook-Pro.local>
2017-05-27 16:50:52 +0700

    commit

--- behaviors/DateBuddhistBehavior.php
+++ behaviors/DateBuddhistBehavior.php
@@ -40,35 +40,17 @@ class DateBuddhistBehavior extends AttributeBehavior
      */
     protected function getValue($event)
     {
+        // echo $this->owner->scenario;
         if ($this->value === null) {
-            // $attribute1 = $this->christianAttribute;
-            // $attribute2 = $this->buddhistAttribute;
-            // $value1 = $this->owner->{$attribute1};
-            // $value2 = $this->owner->{$attribute2};
-            // $explode1 = explode('-', $value1);
-            // $explode2 = explode('/', $value2);
-            // $year1 = intval($explode1[0]);
-            // $year2 = intval($explode2[2]);
-            
-            // $r = null;
-            // switch($event->name) {
-            //     case 'afterFind' : $r = $explode1[2].'/'.$explode1[1].'/'.strval(($year1 + $this->yearDistance));
-            //         break;
-            //     case 'beforeUpdate' : $r = strval(($year2 - $this->yearDistance).'-'.$explode2[1].'-'.$explode2[0]);
-            //         break;
-            //     case 'beforeInsert' : $r = strval(($year2 - $this->yearDistance).'-'.$explode2[1].'-'.$explode2[0]);
-            //         break;
-            // }
-            
-            // return $r;
-            
-            
             $attribute = $this->dateAttribute;
             $value = $this->owner->{$attribute};
             if(!$value){
                 return null;
             }
             $exp = explode('/', $value);
+            if(count($exp) !== 3){
+                return null;
+            }
             $year = intval($exp[2]);
             
             return strval($year - $this->yearDistance).'-'.$exp[1].'-'.$exp[0];
